bba9f5cb2856207aced822dd7a1d5b54b3b613a0
Ray <150236304+Rayxworld@users.noreply.github.com>
2026-02-07 20:54:08 +0100

    Update next.config.js
    next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/:path*',
+        destination: `${process.env.API_BASE_URL}/:path*`,
       },
     ]
   },
